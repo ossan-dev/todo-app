e0ea214fa635bd5df17b7b65ad161ed2723ce151
Ivan Pesenti <ivan-pesenti-dev@ivan-ubuntu>
2021-11-11 12:41:36 +0100

    add validation logic to the UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,25 +4,31 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
-    public function create(Request $request): JsonResponse
+    public function create(Request $request, ValidatorInterface $validator): JsonResponse
     {
         // get payload data
         $parameters = json_decode($request->getContent(), true);
 
         $entity_manager = $this->getDoctrine()->getManager();
-
         $user = new User();
         $user->setFirstName($parameters['first_name']);
         $user->setLastName($parameters['last_name']);
         $user->setEmail($parameters['email']);
 
-        $entity_manager->persist($user);
+        // check if the input is correct
+        $errors = $validator->validate($user);
+        if(count($errors) > 0){
+            throw new BadRequestException((string)$errors);
+        }
 
+        $entity_manager->persist($user);
         $entity_manager->flush();
 
         return $this->json(['message' => "The user with id {$user->getId()} has been saved to db!"]);
