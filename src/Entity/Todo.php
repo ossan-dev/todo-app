6d555cac415eef663756f2c15727a4d35f4485ca
Ivan Pesenti <ivan-pesenti-dev@ivan-ubuntu>
2021-11-11 09:29:21 +0100

    Add Doctrine to proj

---
+++ src/Entity/Todo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_completed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIsCompleted(): ?bool
+    {
+        return $this->is_completed;
+    }
+
+    public function setIsCompleted(bool $is_completed): self
+    {
+        $this->is_completed = $is_completed;
+
+        return $this;
+    }
+}
