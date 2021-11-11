eb4b7e55c869595eade8891cd3fe0af177e4e37c
Ivan Pesenti <ivan-pesenti-dev@ivan-ubuntu>
2021-11-11 11:30:16 +0100

    add Create action under User Endpoint

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    public function create(): JsonResponse
+    {
+        $entity_manager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setFirstName('ivan');
+        $user->setLastName('pesenti');
+        $user->setEmail('ipesenti@sorint.it');
+
+        $entity_manager->persist($user);
+
+        $entity_manager->flush();
+
+        return $this->json(['message' => "The user with id {$user->getId()} has been saved to db!"]);
+    }
 }
