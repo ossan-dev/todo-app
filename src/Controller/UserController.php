001f7dba2da7c292afc1baaee7fa3be008a82e73
Ivan Pesenti <ivan-pesenti-dev@ivan-ubuntu>
2021-11-11 11:37:25 +0100

    removed unused use statements

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,8 +6,6 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
